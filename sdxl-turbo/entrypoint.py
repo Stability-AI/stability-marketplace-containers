8db76d70cdf25a1b5575c62563df59ef648bccfb
satsumas <kate.hodesdon@stability.ai>
2024-05-01 06:01:43 +0000

    remove unused imports and add some minor error handling

--- sdxl-turbo/entrypoint.py
+++ sdxl-turbo/entrypoint.py
@@ -1,4 +1,3 @@
-# main.py
 import base64
 import io
 import logging
@@ -6,15 +5,19 @@ import os
 import sys
 import traceback
  
-import torch
 import uvicorn
 
-from fastapi import FastAPI, HTTPException, Request, Response, status
+from fastapi import FastAPI, HTTPException, Response, status
 from PIL import Image
 from sdxl_turbo import SdxlTurboRequest, setup_pipeline
 
-MODEL_NAME = os.environ["MODEL_NAME"]
-CACHED_MODEL_PATH = os.environ['SAVE_PATH']
+MODEL_NAME = os.getenv("MODEL_NAME")
+CACHED_MODEL_PATH = os.getenv('SAVE_PATH')
+
+if MODEL_NAME is None or CACHED_MODEL_PATH is None:
+    logging.error("Environment variables MODEL_NAME and CACHED_MODEL_PATH must be set. See Dockerfile for values.")
+    sys.exit(1) 
+
 app = FastAPI()
 
 
@@ -51,7 +54,7 @@ async def generate_t2i(request: SdxlTurboRequest):
             media_type="image/png",
         )
     except Exception as e:
-        print(traceback.format_exc())
+        logging.error(traceback.format_exc())
         raise HTTPException(status_code=500, detail=f"Error generating image: {str(e)}")
 
 # sdxl turbo image to image endpoint
@@ -60,8 +63,7 @@ async def generate_i2i(request: SdxlTurboRequest):
     try:
         init_image = request.image
         base64_decoded = base64.b64decode(init_image)
-        input_image = Image.frombytes("RGB", (512, 512), base64_decoded, "raw")
-        
+        input_image = Image.frombytes("RGB", base64_decoded, "raw")
     
         image = pipe_i2i(
             image=input_image,
@@ -72,24 +74,17 @@ async def generate_i2i(request: SdxlTurboRequest):
             num_inference_steps=request.num_inference_steps,
         ).images[0]
 
-        buffer = io.BytesIO()
-        image.save(buffer, format="PNG")
-        return Response(
-            content=buffer.getvalue(),
-            status_code=status.HTTP_200_OK,
-            media_type="image/png",
-        )
+        with io.BytesIO() as buffer:
+            image.save(buffer, format="PNG")
+            return Response(
+                content=buffer.getvalue(),
+                status_code=status.HTTP_200_OK,
+                media_type="image/png",
+            )
     except Exception as e:
         print(traceback.format_exc())
         raise HTTPException(status_code=500, detail=f"Error generating image: {str(e)}")
 
-# def encode_image(image):
-#     buffer = io.BytesIO()
-#     image.save(buffer, format="JPEG")
-#     img_str = base64.b64encode(buffer.getvalue())
-#     return img_str.decode("utf8")
-
-
 if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
-    uvicorn.run("entrypoint:app", host="0.0.0.0", port=port, log_level="debug")
+    uvicorn.run("entrypoint:app", host="0.0.0.0", port=port)
